f9e2fb0422676267bd29db92f6531c2509117a31
Andre' Taylor <totlpkg170@outlook.com>
2015-09-01 15:39:25 -0400

    Added Game Eastser Egg

---
+++ ElevenNote.Web/Models/GuessingGameViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.Web.Models
+{
+    public class GuessingGameViewModel
+    {
+        [Required(ErrorMessage ="Required")]
+        [MinLength(2)]
+        [MaxLength(20)]
+        [Display(Name = "Your Name")]
+        public String Name { get; set; }
+        
+        [Required(ErrorMessage ="Required")]
+        [Range(1,10)]
+        [Display(Name = "Your Guess")]
+        public int Guess { get; set;}                 
+    }
+}
